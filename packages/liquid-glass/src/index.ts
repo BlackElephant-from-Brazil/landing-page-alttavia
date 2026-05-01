e0c282065b84a29c294deb94dd294df8ecea591f
omelleca <melle.miguel1@gmail.com>
2026-04-30 21:02:12 -0300

    feat(liquid-glass): export widgets and debug from index

--- packages/liquid-glass/src/index.ts
+++ packages/liquid-glass/src/index.ts
@@ -1,4 +1,22 @@
+// Core component
 export { LiquidGlass, LiquidGlassShell, LiquidGlassDefs } from "./components/LiquidGlass";
 export type { LiquidGlassProps } from "./components/LiquidGlass";
+
+// Widgets
+export { LensGlass } from "./components/widgets/LensGlass";
+export type { LensGlassProps } from "./components/widgets/LensGlass";
+export { SearchboxGlass } from "./components/widgets/SearchboxGlass";
+export type { SearchboxGlassProps } from "./components/widgets/SearchboxGlass";
+export { SwitchGlass } from "./components/widgets/SwitchGlass";
+export type { SwitchGlassProps } from "./components/widgets/SwitchGlass";
+export { SliderGlass } from "./components/widgets/SliderGlass";
+export type { SliderGlassProps } from "./components/widgets/SliderGlass";
+export { PlayerGlass } from "./components/widgets/PlayerGlass";
+export type { PlayerGlassProps } from "./components/widgets/PlayerGlass";
+
+// Debug
+export { DebugOverlay } from "./debug/DebugOverlay";
+
+// Types
 export type { SpecularOptions, MapGenResult } from "./gl/generate-map";
 export type { FeatureSupport } from "./hooks/useFeatureDetect";
